734d63379d15c37a19993d62ddc73b895a5eceb2
Regis <regis.lopes.eso@gmail.com>
2025-03-19 10:51:42 -0300

    Add class 123. Variables and Data Types in Javascript

--- JS_Starter/script.js
+++ JS_Starter/script.js
@@ -1,1 +1,12 @@
 console.log("Hello from separated file");
+
+let name1 = "Alex";
+let age = 19;
+let salary = age * 200;
+let isMarried = false;
+
+console.log(
+  `His name is ${name1}, his age is ${age}. And he is married, right? ${
+    isMarried == true ? "True!" : "Nope!"
+  }. His salary $${salary},00`
+);
