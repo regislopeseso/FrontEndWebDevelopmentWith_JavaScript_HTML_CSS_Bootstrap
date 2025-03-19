dd91253fdad04a5192178f9aa00042a451298871
Regis <regis.lopes.eso@gmail.com>
2025-03-19 11:07:13 -0300

    Add class 124. Type Coercion in Javascript

--- JS_Starter/script.js
+++ JS_Starter/script.js
@@ -10,3 +10,16 @@ console.log(
     isMarried == true ? "True!" : "Nope!"
   }. His salary $${salary},00`
 );
+0;
+
+var job;
+console.log(job);
+
+job = "unemployed";
+console.log(job);
+
+job = 123;
+job = 2 * job;
+job++;
+console.log(job);
+console.log(job % 2);
